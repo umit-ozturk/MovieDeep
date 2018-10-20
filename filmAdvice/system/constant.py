6dff348c7abebcd0a0d8f561be4e473c551bfad0
Ümit Öztürk <1150606004@nku.edu.tr>
2018-10-20 13:42:27 +0300

    Git İngore File Updated

---
+++ filmAdvice/system/constant.py
@@ -0,0 +1,4 @@
+SYSTEM_APP_PATH = '/filmAdvice/system/'
+DATASET_MOVIES_FILE = 'ratings.dat'
+DATASET_RATINGS_FILE = 'ratings.dat'
+DATASET_TAGS_FILE = 'ratings.dat'
--- filmAdvice/system/load_data.py
+++ filmAdvice/system/load_data.py
@@ -1,16 +1,44 @@
 import csv
-import os
+from filmAdvice.settings import APP_MAIN_CURRENT_PATH
+from filmAdvice.system.constant import *
 from filmAdvice.movie.models import Movie
 
 
-def load_movie_data():
-    current_pat = os.getcwd()
-    with open(current_pat + '/filmAdvice/system/movies.dat', encoding='utf-8') as f:
-        reader = csv.DictReader([line.replace('::', '\t') for line in f],
-                                fieldnames='MovieID::Title::Genres'.split('::'), delimiter='\t')
+class LoadDataSets:
+    def __init__(self):
+        self.load_movie_data()
+        self.load_rating_data()
+        self.load_tag_data()
 
-    for row in reader:
-        print(row)
-        # m = Movie(title=row['Title'], genre=row['Genres'])
-        # m.save()
-    return reader
+    def load_movie_data(self):
+        with open(APP_MAIN_CURRENT_PATH + SYSTEM_APP_PATH + DATASET_MOVIES_FILE, encoding='utf-8') as f:
+            reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                    fieldnames='MovieID::Title::Genres'.split('::'), delimiter='\t')
+
+        for row in reader:
+            print(row)
+            # movie = Movie(title=row['Title'], genre=row['Genres'])
+            # movie.save()
+        return reader
+
+    def load_rating_data(self):
+        with open(APP_MAIN_CURRENT_PATH + SYSTEM_APP_PATH + DATASET_RATINGS_FILE, encoding='utf-8') as f:
+            reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                    fieldnames='MovieID::Title::Genres'.split('::'), delimiter='\t')
+
+        for row in reader:
+            print(row)
+            # rating = Movie(title=row['Title'], genre=row['Genres'])
+            # rating.save()
+        return reader
+
+    def load_tag_data(self):
+        with open(APP_MAIN_CURRENT_PATH + SYSTEM_APP_PATH + DATASET_TAGS_FILE, encoding='utf-8') as f:
+            reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                    fieldnames='MovieID::Title::Genres'.split('::'), delimiter='\t')
+
+        for row in reader:
+            print(row)
+            # user = Movie(title=row['Title'], genre=row['Genres'])
+            # user.save()
+        return reader
