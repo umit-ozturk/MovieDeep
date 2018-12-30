48a9a458cef8da863a3a4efb43779e1fbe3b20f9
Ümit Öztürk <1150606004@nku.edu.tr>
2018-12-30 21:21:48 +0300

    Rec Engine Update

--- filmAdvice/system/constant.py
+++ filmAdvice/system/constant.py
@@ -3,3 +3,22 @@ DATASET_LINKS_FILE = 'links.csv'
 DATASET_MOVIES_FILE = 'movies.csv'
 DATASET_RATINGS_FILE = 'ratings.csv'
 DATASET_TAGS_FILE = 'tags.csv'
+
+n_nodes_inpl = 1260
+n_nodes_hl1 = 100
+n_nodes_outl = 1260
+
+NODES_CONST = (
+    (n_nodes_inpl, 1260),
+    (n_nodes_hl1, 100),
+    (n_nodes_outl, 1260),
+)
+
+batch_size = 100
+hm_epochs = 50
+
+TENSOR_CONST = (
+    (n_nodes_inpl, 1260),
+    (n_nodes_hl1, 100),
+    (n_nodes_outl, 1260),
+)
--- filmAdvice/system/recomender_engine.py
+++ filmAdvice/system/recomender_engine.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error as MSE
+from filmAdvice.system.constant import *
 
 
 DATASET_RATINGS_FILE = "filmAdvice/system/try_ratings.csv"
@@ -11,21 +12,13 @@ DATASET_RATINGS_FILE = "filmAdvice/system/try_ratings.csv"
 
 def recomender_engine():
     ratings = pd.read_csv(DATASET_RATINGS_FILE, sep=",", engine='python')
-    print(ratings.shape)
     ratings_pivot = pd.pivot_table(ratings[['userId', 'movieId', 'rating']], values='rating',
                                    index='userId', columns='movieId').fillna(0)
-    print(ratings_pivot)
     x_train, x_test = train_test_split(ratings_pivot, train_size=0.8)
-    print(x_train)
-    n_nodes_inpl = 1260
-    n_nodes_hl1 = 100
-    n_nodes_outl = 1260
     hidden_1_layer_vals = {'weights': tf.Variable(tf.random_normal([n_nodes_inpl+1, n_nodes_hl1]))}
     output_layer_vals = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1+1, n_nodes_outl]))}
     input_layer = tf.placeholder('float', [None, 1260])
-    print(input_layer)
     input_layer_const = tf.fill([tf.shape(input_layer)[0], 1], 1.0)
-    print(input_layer_const)
     input_layer_concat = tf.concat([input_layer, input_layer_const], 1)
     layer_1 = tf.nn.sigmoid(tf.matmul(input_layer_concat, hidden_1_layer_vals['weights']))
     layer1_const = tf.fill([tf.shape(layer_1)[0], 1], 1.0)
@@ -35,30 +28,34 @@ def recomender_engine():
     meansq = tf.reduce_mean(tf.square(output_layer - output_true))
     learn_rate = 0.1
     optimizer = tf.train.AdagradOptimizer(learn_rate).minimize(meansq)
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
+    init, sess = init_tensor_sess()
     sess.run(init)
-    batch_size = 100
-    hm_epochs = 200
-    tot_users = x_train.shape[0]
-    print(tot_users)
-    sess = tf.Session()
-    sess.run(init)
-    batch_size = 100
-    hm_epochs = 50
     tot_users = x_train.shape[0]
     for epoch in range(hm_epochs):
         epoch_loss = 0  # initializing error as 0
         for i in range(int(tot_users / batch_size)):
             epoch_x = x_train[i * batch_size: (i + 1) * batch_size]
             _, c = sess.run([optimizer, meansq], feed_dict={input_layer: epoch_x, output_true: epoch_x})
-            print("Debug4")
             epoch_loss += c
         output_train = sess.run(output_layer, feed_dict={input_layer: x_train})
         output_test = sess.run(output_layer, feed_dict={input_layer: x_test})
 
         print('MSE train', MSE(output_train, x_train), 'MSE test', MSE(output_test, x_test))
         print('Epoch', epoch, '/', hm_epochs, 'loss:', epoch_loss)
+    return x_train, x_test, input_layer, output_layer
+
+
+def init_tensor_sess():
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    sess = tf.Session()
+    return init, sess
+
+
+def take_predict(x_train):
+    x_train, x_test, input_layer, output_layer = recomender_engine()
+    init, sess = init_tensor_sess()
     sample_user = x_test.iloc[11, :]
     print(sample_user)
     sample_user_pred = sess.run(output_layer, feed_dict={input_layer: [sample_user]})
